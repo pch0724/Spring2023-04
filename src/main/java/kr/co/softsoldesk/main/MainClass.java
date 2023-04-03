3c19d5e415b739eeb0bace4594ba63e4ca414f0d
soldesk <soldesk@DESKTOP-3D2JIQJ>
2023-04-03 17:56:51 +0900

    0403

--- src/main/java/kr/co/softsoldesk/main/MainClass.java
+++ src/main/java/kr/co/softsoldesk/main/MainClass.java
@@ -1,24 +1,72 @@
 package kr.co.softsoldesk.main;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
-import kr.co.softsoldesk.beans.HelloWorld;
+import kr.co.softsoldesk.beans.TestBean;
 
 public class MainClass {
 
 	public static void main(String[] args) {
+		//test1();
 		
+		//test2();
+		
+		//test3();
+		
+		test4();
+	}
+	
+	public static void test1() {
+		// 경로지정 객체
+		ClassPathResource res = new ClassPathResource("kr/co/softsoldesk/config/beans.xml");
+		// 경로지정 객체를 xml 생성
+		XmlBeanFactory factory = new XmlBeanFactory(res);
+		TestBean t1 = factory.getBean("t1", TestBean.class);
+		System.out.println("t1 : " + t1);
+		TestBean t2 = factory.getBean("t1", TestBean.class);
+		System.out.println("t2 : " + t2);
+	}
+
+	public static void test2() {
+		// 외부 패키지
+		FileSystemResource res = new FileSystemResource("beans.xml");
+		// 경로지정 객체를 xml 생성
+		XmlBeanFactory factory = new XmlBeanFactory(res);
+		TestBean t2 = factory.getBean("t2", TestBean.class);
+		System.out.println("t2 : " + t2);
+		TestBean t1 = factory.getBean("t2", TestBean.class);
+		System.out.println("t1 : " + t1);
+	}
+	//test1()의 확장 코드
+	public static void test3() {
+		// 자동으로 생성자 생성
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/co/softsoldesk/config/beans.xml");
 		
-		HelloWorld hello1 = (HelloWorld)ctx.getBean("hello1");
-		callhello(hello1);
+		
+		TestBean t2 = ctx.getBean("t1", TestBean.class);
+		System.out.println("t2 : " + t2);
+		TestBean t1 = ctx.getBean("t1", TestBean.class);
+		System.out.println("t1 : " + t1);
+		
+		//ctx.close();
+	}
+	
+	//test2()의 확장 코드
+	public static void test4() {
+		// 외부 패키지
+		FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("beans.xml");
+				
+		TestBean t2 = ctx.getBean("t2", TestBean.class);
+		System.out.println("t2 : " + t2);
+		
+		TestBean t1 = ctx.getBean("t2", TestBean.class);
+		System.out.println("t1 : " + t1);
+		
 		ctx.close();
 	}
 	
-	public static void callhello(HelloWorld hello) {
-		hello.sayHello();
-	}
-	
 }
-
-// ClassPathXmlApplicationContext : 인스턴스 변수(객체)를 편리하게 생성하는 클래스
